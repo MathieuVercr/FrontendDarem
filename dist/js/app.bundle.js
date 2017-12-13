0915b1040ac9a4b3bf09099277954e27ce6bdae5
Mathieu Vercruysse <vercruysse.mathieu@outlook.com>
2017-12-13 12:18:37 +0100

    facebook login

--- dist/js/app.bundle.js
+++ dist/js/app.bundle.js
@@ -78,11 +78,21 @@ module.exports = __webpack_require__(2);
 "use strict";
 
 
+(function (d, s, id) {
+  var js,
+      fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);js.id = id;
+  js.src = 'https://boiling-dawn-34823.herokuapp.com/users/auth/facebook';
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, 'script', 'facebook-jssdk');
+
 // SHOW AND HIDE POPUP
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
   var popup = document.getElementById("signup");
+  var fbLogin = document.getElementsByClassName("loginBtn--facebook")[0];
 
   var signups = document.getElementsByClassName("startNow");
   for (var i = 0; i < signups.length; i++) {
@@ -101,6 +111,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
     if (element.target == popup) {
       popup.style.display = "none";
     }
+  });
+
+  fbLogin.addEventListener("click", function () {
+    window.location.href = "https://boiling-dawn-34823.herokuapp.com/users/auth/facebook";
   });
 });
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,17 @@
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://boiling-dawn-34823.herokuapp.com/users/auth/facebook';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 // SHOW AND HIDE POPUP
 
 document.addEventListener("DOMContentLoaded", (event) => {
 
   var popup = document.getElementById("signup");
+  var fbLogin = document.getElementsByClassName("loginBtn--facebook")[0];
 
   var signups = document.getElementsByClassName("startNow");
   for(var i=0; i<signups.length; i++){
@@ -22,5 +31,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
     if (element.target == popup) {
         popup.style.display = "none";
     }
+  });
+
+  fbLogin.addEventListener("click", () => {
+    window.location.href = "https://boiling-dawn-34823.herokuapp.com/users/auth/facebook";
   });
 });
